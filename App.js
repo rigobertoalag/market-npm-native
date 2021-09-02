b8c69dd981f8c027c60c8c04e768651507f9965d
rigobertoalag <a20150338@utem.edu.mx>
2021-09-02 11:43:23 -0500

    Integracion correcta del drawernavigator

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React, { useState } from 'react';
-import { Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -11,38 +10,39 @@ import Login from './screens/auth/Login';
 import Register from './screens/auth/Register';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function Root() {
+  return (
+    <Drawer.Navigator initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff'
+      }}>
+      <Drawer.Screen name="Home" component={HomeScreen} options={{ title: 'Bienvenido' }} />
+      <Drawer.Screen name="login" component={Login} options={{ title: 'Inicio de sesion' }} />
+      <Drawer.Screen name="register" component={Register} options={{ title: 'Registro' }} />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
-  const [logged, isLogged] = useState(false)
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff'
-        }}>
-        <Stack.Screen name="Home" component={HomeScreen} options={{
-          title: 'Bienvenido',
-          headerRight: () => (
-            logged ? 
-            <Button
-              onPress={() => {isLogged(false); alert('Cerraste sesion')}}
-              title="Cerrar sesion"
-              color="blue"
-            />
-            :
-            <Button
-              onPress={() => {isLogged(true); alert('Iniciaste sesion')}}
-              title="Iniciar sesion"
-              color="blue"
-            />
-          ),
-        }} />
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff'
+      }}>
+        <Stack.Screen
+          name="Root"
+          component={Root}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="login" component={Login} options={{ title: 'Inicio de sesion' }} />
-        <Stack.Screen name="register" component={Register} options={{ title: 'Registro' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
